c4ac40fc60390d3aad1b9a4dc2a4f0aea13cb5d0
Sandra Rivera <sandrarivera@ciencias.unam.mx>
2024-10-23 11:15:12 -0600

    consenso con lider cero

--- Practica_4_Consenso/src/NodoConsenso.py
+++ Practica_4_Consenso/src/NodoConsenso.py
@@ -1,6 +1,8 @@
 import simpy
+from random import randint
 from Nodo import *
 from Canales.CanalRecorridos import *
+import math
 
 # La unidad de tiempo
 TICK = 1
@@ -17,11 +19,40 @@ class NodoConsenso(Nodo):
         # Atributos extra
         self.V = [None] * (len(vecinos) + 1) # Llenamos la lista de Nodos
         self.V[id_nodo] = id_nodo
-        self.New = set([id_nodo])
+        self.New = set()
         self.rec_from = [None] * (len(vecinos) + 1)
         self.fallare = False      # Colocaremos esta en True si el nodo fallará
         self.lider = None         # La elección del lider.
+        self.fallo = math.inf
+        self.New.add((0, id_nodo))
 
     def consenso(self, env, f):
         '''El algoritmo de consenso.'''
-        # Aquí va su implementación
+        # Escoger cuando fallarán los nodos.
+        if self.id_nodo < f:
+            self.fallo = randint(0, f)
+            self.fallare = True
+
+        while env.now <= f:
+            if self.New != set() and env.now < self.fallo:
+                #print(self.New)
+                #print(self.id_nodo)
+                self.canal_salida.envia([self.New, self.id_nodo], self.vecinos)
+            [rec, p] = yield self.canal_entrada.get()
+            #print(rec)
+            #print(p)
+            self.rec_from[p] = rec
+            self.New = set()
+            for j in self.vecinos:
+                if self.rec_from[j] != None:
+                    for i in self.rec_from[j]:
+                        [v,k] = i
+                        if self.V[k] == None:
+                            self.V[k] = v
+                            self.New.add((v, k))
+            yield env.timeout(TICK)
+
+        for v in self.V:
+            if v != None:
+                self.lider = v
+        self.lider = None
